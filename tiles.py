52e959b3575c642f3639aa72227a7971691b293d
Svanhvit19 <54936676+Svanhvit19@users.noreply.github.com>
2019-09-17 11:06:38 +0000

    Add files via upload

--- tiles.py
+++ tiles.py
@@ -2,5 +2,38 @@
 x = 1
 y = 1
 #input
-direction = input("Direction: ")
+
 #move
+def north(x,y):
+    y += 1
+    return (x,y)
+
+def south(x,y):
+    y -= 1
+    return x,y
+
+def east(x,y):
+    x += 1
+    return (x,y)
+
+def west(x,y):
+    x -= 1
+    return x,y
+
+direction = input("Direction: ")
+#while 0 < x <= 3 and 0 < y <= 3:
+if direction == "N" or "n":
+    place = north(x,y)
+    print(place)
+elif direction == "S" or "s":
+    place = south(x,y)
+    print(place)
+elif direction == "E" or "e":
+    place = east(x,y)
+    print(place)
+elif direction == "W" or "w":
+    place = west(x,y)
+    print(place)
+else:
+    print("Not a valid direction")
+    #direction = input("Direction: ")
